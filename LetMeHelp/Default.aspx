83b72bf865d35f849bc6ab1cd18e8556e966810b
SK0P3iN <juan_silva1997@live.com.pt>
2021-01-26 14:33:14 +0000

    Data base fixed and default page updated

--- LetMeHelp/Default.aspx
+++ LetMeHelp/Default.aspx
@@ -4,41 +4,18 @@
 
 
     <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+        <h1>LetMeHelp</h1>
+        <p class="lead">Aqui encontrará formas de ajudar o próximo e de pedir ajuda sem ser necessário pagamentos munetários.</p>
     </div>
 
-    <div class="row">
+    <h2>Anúncios</h2>
+    <hr />
 
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
+    <div id="DivNoPosts" runat="server">
+        <p id="NoPostInfo">There are currently no posts to show.</p><br />
+        <p>Thank you for using our service.</p>
     </div>
 
+    <section id="postsDedicated" style="text-align:center" runat="server">
+    </section>
 </asp:Content>
--- LetMeHelp/Default.aspx.cs
+++ LetMeHelp/Default.aspx.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using LetMeHelp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace LetMeHelp
@@ -11,7 +13,62 @@ namespace LetMeHelp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (var db = new ModelLMH())
+            {
+                //var teste = db.Utilizadores.First();
+                //db.Posts.Add(new Post() { PostId = 1, PostDate = DateTime.Now, PostHeader = "Acartador de Batatas", PostDescription = "Preciso de alguem para acartar batatas em troca de 30kg das mesmas", UserId = 50, User= teste });
+                //db.SaveChanges();
 
+                if (db.Posts.Count() == 0)
+                {
+
+                }
+                else
+                {
+                    DivNoPosts.Visible = false;
+
+
+                    foreach (var post in db.Posts.ToList().OrderBy(p => p.PostDate))
+                    {
+                        HtmlGenericControl example = new HtmlGenericControl("div");
+
+                        HtmlGenericControl title = new HtmlGenericControl("h2");
+                        title.InnerText = post.PostHeader;
+                        title.ID = post.PostId.ToString();
+
+                        #region Criar um <a href>
+
+                        HtmlGenericControl redirector = new HtmlGenericControl("a");
+                        redirector.InnerHtml = $"<a href=veranuncio?postId={post.PostId}>";
+
+                        #endregion
+
+
+                        HtmlImage image = new HtmlImage();
+
+                        image.Src = post.PostImg;
+                        image.Alt = "Post Image";
+
+                        image.Height = 100;
+                        image.Width = 100;
+
+                        #region Add Elements of Post
+
+                        redirector.Controls.Add(title);
+                        redirector.Controls.Add(image);
+
+                        example.Controls.Add(redirector);
+
+                        //example.Controls.Add(title);
+                        //example.Controls.Add(image);
+
+                        #endregion
+
+                        postsDedicated.Controls.Add(example);
+                        postsDedicated.Controls.Add(new HtmlGenericControl("br"));
+                    }
+                }
+            }
         }
     }
 }
